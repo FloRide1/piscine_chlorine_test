db22da3a01cab1ee4fd3c8d25fb4f84727a56af3
Florian FloRide Reimat <florian.reimat@epita.fr>
2021-10-01 13:51:23 +0200

    Make the script v1.0

--- create_directory.sh
+++ create_directory.sh
@@ -1,8 +1,48 @@
 #!/bin/sh
 
-NAME=default
-PISCINE_FOLDER=
+#######################
+# DEFINE:             #
+#######################
+#NAME="<your_name>"
+#PISCINE_FOLDER="$HOME/afs/<your_piscine_path>"
+#SCRIPT_PATH="$HOME/afs/<your_chlorine_path>"
+#
+# Exemple:            
+#   NAME="flo"
+#   PISCINE_FOLDER="$HOME/afs/Documents/Piscine/EXERCISES"
+#   SCRIPT_PATH="$HOME/afs/Documents/Piscine/piscine_chlorine_test"
+#######################
+
+FOLDER=""
+PROJECT=""
+REGEXEND="_test.c"
 
 if [ $# -ge 1 ]; then
-    while [ $]
-    
+    while [ $# -ne 0 ]; do
+        if [ "${FOLDER}" == "" ]; then
+            FOLDER=$1
+        else
+            PROJECT=$1
+        fi
+
+        shift
+    done
+
+    cd $HOME
+
+    if [ "$FOLDER" != "" ]; then
+        FOLDER_NAME="$SCRIPT_PATH/$FOLDER"
+        mkdir $FOLDER_NAME
+        if [ "$PROJECT" != "" ]; then
+            SCRIPT="$FOLDER_NAME/${PROJECT}_${NAME}$REGEXEND"
+        else
+            SCRIPT="$FOLDER_NAME/${NAME}$REGEXEND"
+        fi
+
+        touch "$SCRIPT"
+
+        mkdir "$PISCINE_FOLDER/$FOLDER"
+        ln -s $SCRIPT "$PISCINE_FOLDER/$FOLDER"
+
+    fi
+fi
